a03bfee37f6b82621bd7228ae1493a7949b29c34
wen <wisakura@outlook.com>
2023-12-29 20:55:41 +0800

    update style

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,19 @@ import { Section } from '~/components/section';
 
 export default function Home() {
   return (
-    <main className='min-h-screen relative p-2 md:pt-16 md:pl-24 md:space-y-8 space-y-4'>
+    <main className='min-h-screen relative p-2 md:pt-24 md:pl-48 md:space-y-8 space-y-4'>
       <Image
         fill
         className='z-50 pointer-events-none opacity-50 dark:opacity-5'
         src='/zd.png'
         alt='zd'
       />
-      <h1
-        className={clsx(
-          'text-5xl md:text-7xl inline-block font-bold text-transparent bg-clip-text bg-gradient-to-tr from-sky-950 to-sky-50 ',
-          'dark:from-green-400'
-        )}>
-        Akumanoko
+      <h1 className={clsx('text-5xl  md:text-8xl inline-block font-black  text-blue-700  ', 'dark:text-green-400')}>
+        AKUMA
+        <br />
+        NO KO
       </h1>
-      <p>平平无奇小博客。</p>
+      <p>在我成为大文豪之前，你们可以免费阅读我的博客。</p>
       <div className='flex space-x-4'>
         <Social />
         <ThemeToggle />
