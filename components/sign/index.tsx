e5f1963edb5e42b0584d442a6461ccfab29a756b
wen <wisakura@outlook.com>
2024-03-25 00:59:10 +0800

    fix: fix sign component ui

--- components/sign/index.tsx
+++ components/sign/index.tsx
@@ -9,10 +9,10 @@ export function Sign(props: PropsWithChildren) {
   return (
     <div className='p-2 -ml-2 hover:bg-zinc-950/10  dark:hover:bg-zinc-50/10  rounded-lg w-fit'>
       <div className='sign mix-blend-luminosity'>
-        <p className='text-lg leading-loose whitespace-pre-line absolute top-t left-0 '>
+        <p className='text-lg leading-loose whitespace-pre-line absolute top-t left-0 pr-2'>
           <Typeit>{props.children}</Typeit>
         </p>
-        <p className='text-lg leading-loose whitespace-pre-line inline-block opacity-10 select-none selection:text-transparent pointer-events-none'>
+        <p className='text-lg leading-loose whitespace-pre-line inline-block opacity-10 select-none selection:text-transparent pointer-events-none '>
           <span>{props.children}</span>
         </p>
       </div>
